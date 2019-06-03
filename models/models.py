abef12544311f1e4b0c03e1bdc0fdd88626a165c
ccysfn <jessecao911@gmail.com>
2019-06-03 15:22:43 +0800

    we

--- models/models.py
+++ models/models.py
@@ -2,8 +2,7 @@
 
 from odoo import models, fields, api
 
-class Student(models.Model):
-    _name = 'vschool.student'
+class res_partner(models.Model):
     _inherit = 'res.partner'
     grade = fields.Char()
 #     value = fields.Integer()
@@ -13,3 +12,8 @@ class Student(models.Model):
 #     @api.depends('value')
 #     def _value_pc(self):
 #         self.value2 = float(self.value) / 100
+
+class student(models.Model):
+    _name = 'vschool.student'
+    _inherits = {'res.partner','partner_id'}
+    ddd = fields.Char()
